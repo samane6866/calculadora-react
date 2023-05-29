1744ca1e832693bb84015ff8a406083c9aeb3afa
samane6866 <samane.bahramian@gmail.com>
2023-05-29 19:07:16 +0200

    create Boton component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Boton from "./components/Boton";
 
 function App() {
+  <Boton>1</Boton>;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenedor-calculadora">
+        <div className="fila"></div>
+        <div className="fila"></div>
+        <div className="fila"></div>
+        <div className="fila"></div>
+        <div className="fila"></div>
+      </div>
     </div>
   );
 }
