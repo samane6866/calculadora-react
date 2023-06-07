d466a9721bdff54219a39afb48722262121e48dd
samane6866 <samane.bahramian@gmail.com>
2023-06-07 23:03:47 +0200

    create some new component s,botonClear/Input/css

---
+++ src/components/BotonClear.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../hojas-de-estilo/BotonClear.css";
+
+
+const BotonClear = (props,children) => (
+    <div className="boton-clear">
+
+{props.children}
+
+    </div>
+
+
+
+
+)
+export default BotonClear;
---
+++ src/components/Pantalla.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../hojas-de-estilo/PantallasInput.css"
+function Pantalla({input}) {
+    return (
+        <div className="input">
+    {input}
+</div>
+    )
+    
+
+
+
+
+
+}
+export default Pantalla;
